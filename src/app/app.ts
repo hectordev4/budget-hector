636ff2af689dbab2b853ae699717f83dca235121
hectordev4 <125360015+hectordev4@users.noreply.github.com>
2026-07-13 13:16:48 +0200

    feat: add card component, fix: banner folder route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Banner } from './banner/banner';
+import { Banner } from './components/banner/banner';
 
 @Component({
   selector: 'app-root',
