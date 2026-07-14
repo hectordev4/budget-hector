62371db2c952eb381e6fa103108ecf888594dbf4
hectordev4 <125360015+hectordev4@users.noreply.github.com>
2026-07-14 12:56:42 +0200

    feat: implement Overview component with budget calculation and selection tracking feat: implement type Offer into Overview

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Banner } from './components/banner/banner';
-import { ItemCard } from './components/card/card';
+import { Overview } from './pages/overview/overview';
 
 @Component({
   selector: 'app-root',
-  imports: [Banner, ItemCard],
+  imports: [Banner, Overview],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
