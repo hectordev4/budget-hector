53e8fd74c1756be53f377582d3d23a89d1d6bd85
hectordev4 <125360015+hectordev4@users.noreply.github.com>
2026-07-14 10:20:37 +0200

    feat: implement card component with Angular Material styling

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule],
   templateUrl: './card.html',
   styleUrl: './card.css',
 })
-export class Card {}
+export class Card {
+
+}
